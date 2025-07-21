f78e5c132935890d22853fa02fedea0d2f8cc477
ajay1762t <nidhisharma545556@gmail.com>
2025-07-21 19:57:11 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to HALF ALP</h1>
+      <p>Your online test series platform for RRB ALP</p>
+    </div>
+  );
+}
